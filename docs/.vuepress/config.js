385a2cb82f4234e296c2e03968e1e745e9771345
hushenbao <15926502600@163.com>
2020-05-10 13:28:42 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "胡神宝的博客",
+    head: [
+        ['link', {rel: 'icon', href: 'http://wx3.sinaimg.cn/mw690/b771bb24gy1fnod2r8e8jj207i0aiab3.jpg'}]
+    ],
+    themeConfig: {
+        logo: 'http://wx3.sinaimg.cn/mw690/b771bb24gy1fnod2r8e8jj207i0aiab3.jpg',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '技术文档', link: '#'},
+            {text: '简书主页', link: 'https://www.jianshu.com'}
+        ],
+        sidebar: 'auto'
+    }
+
+}
